40130836428246743ca43e47f4a6def7c40aa5c3
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-11-27 00:07:23 -0300

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -1,12 +1,12 @@
 #!/bin/env bash
 DEBIAN_FRONTEND=noninteractive
-sudo apt update -qq 
+sudo apt update
 sudo apt install -y gnupg wget git curl screen ubuntu-server dos2unix cmake make
-sudo apt clean
 wget https://github.com/aptly-dev/aptly/releases/download/v1.4.0/aptly_1.4.0_amd64.deb -O /tmp/aptly.deb 
-sudo dpkg -i /tmp/aptly.deb || exit 127
+sudo dpkg -i /tmp/aptly.deb
 sudo -rfv /tmp/aptly.deb
 
 sudo mkdir -p /aptly
 chown $USER:$GROUP /aptly
 chown 777 /aptly
+exit 0
